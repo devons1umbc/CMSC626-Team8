138cc7d9667287425d65d496dc5beaf2d8c4f777
Devon Slonaker <devons1@umbc.edu>
2023-11-30 03:47:49 -0500

    Overhauled file system
    - Cleaned up code
    - Made write faster by removing unnecessary code
    - Added mutex for write to deal with concurrent write

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ arg.add_argument("-s", "--search", type=str, help="Search for a file")
 arg.add_argument("-d", "--download", type=str, help="Download a file")
 arg.add_argument("-r", "--read", type=str, help="Read a file")
 arg.add_argument("-c", "--create", type=str, help="Create a file")
-arg.add_argument("-w", "--write", type=str, help="Write to a file")
-arg.add_argument("-m", "--message", type=str, help="Message to write to a file")
+arg.add_argument("-w", "--write", type=str, help="Write to a file (must be used with -m)")
+arg.add_argument("-m", "--message", type=str, help="Message to write to a file (to be used with -w)")
 
 args = arg.parse_args()
 
-directory_server = "192.168.1.6"
+directory_server = "192.168.1.4"
 
 
 def getip():
@@ -28,120 +28,185 @@ def getip():
 
 
 def search(query):
+    ping = os.popen("ping -c 1 -w 1 " + str(directory_server)).read()
+    if "100% packet loss" in ping:
+        return 2
     files = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " ls /home/cmsc626/Desktop/files").read().split('\n')
     for file in files:
-        print(file)
         if file.lower() == str(query).lower():
             users = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " ls /home/cmsc626/Desktop/files/" + file).read().split('\n')
             for user in users:
                 if user != '':
-                    print(user)
                     ping = os.popen("ping -c 1 -w 1 " + str(user)).read()
-                    if "100% packet loss" in ping:
-                        print("Could not connect to " + user)
-                    else:
-                        print("File " + query + " has been found")
+                    if "100% packet loss" not in ping:
                         return [user, file]
     return 0
 
 
 def read(query):
     location = search(query)
-    print(location)
-    if location:
-        if location[0] == socket.gethostbyname(socket.gethostname() + "."):
+    if location and location != 2:
+
+        # If the current user is the one who owns the most recent file
+        if location[0] == getip():
             return os.popen("cat files/"+location[1]+"/"+location[1]).read()
         else:
-            return os.popen("sshpass -p 12345 ssh cmsc626@" + location[0] + " cat /home/cmsc626/Desktop/" + location[1]).read()
+            return os.popen("sshpass -p 12345 ssh cmsc626@" + location[0] + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + location[1]).read()
     else:
-        print("File " + query + " not found")
         return 0
 
 
-def create(file):
-    if search(file):
-        print(file + " already exists.")
+def create(query):
+    # Fail if file already exists
+    if search(query) and search(query) != 2:
         return 0
     else:
         ip = getip()
-        os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " mkdir /home/cmsc626/Desktop/files/" + file ).read()
-        os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " touch /home/cmsc626/Desktop/files/" + file + "/" + ip).read()
         # Combine everything cause race conditions
-        os.popen("mkdir " + "files/" + file + " && " + "touch " + "files/" + file + "/" + file + " && " + "touch " + "files/" + file + "/" + ".version" + " && "
-                + "echo \'1\n" + ip + "\' > " + "files/" + file + "/" + ".version" + " && "
-                + "sshpass -p 12345 rsync files/" + file + "/" + ".version" + " cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + file + "/.version")
+        # Establish new file's presence on directory server and make file locally
+        os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " mkdir /home/cmsc626/Desktop/files/" + query +
+                 " && sshpass -p 12345 ssh cmsc626@" + directory_server + " touch /home/cmsc626/Desktop/files/" + query + "/" + ip +
+                 " && mkdir " + "files/" + query + " && " + "touch " + "files/" + query + "/" + query + " && " + "touch " + "files/" + query + "/" + ".version" +
+                 " && echo \'1\n" + ip + "\' > " + "files/" + query + "/" + ".version" +
+                 " && sshpass -p 12345 rsync files/" + query + "/" + ".version" + " cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + query + "/.version").read()
         return 1
 
 
-def write(file, text):
-    location = search(file)
-    if location:
+# THIS IS DEPRECATED BUT WE HAVE DECIDED TO KEEP THIS HERE FOR LEGACY PURPOSES AND TO SEE OUR THOUGHT PROCESS
+# Make changes locally, check version, push changes. Revert if wrong
+def write(query, text):
+    location = search(query)
+    if location and location != 2:
+        # Get initial version
         version = open("files/" + location[1] + "/" + ".version").read().split()
         print(version[0])
         ip = getip()
 
+        # Modify the file to reflect changes
         newfile = open("files/" + location[1] + "/" + location[1], "w")
         newfile.write(text)
         newfile.close()
 
+        # Modify version to reflect changes
         updated = open("files/" + location[1] + "/" + ".version", "w")
         updated.write(str(int(version[0])+1) + "\n" + ip)
         updated.close()
 
+        # Get new version number
         version = open("files/" + location[1] + "/" + ".version").read().split()
         print(version)
-        remoteversion = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
+        remote_version = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
 
-        print(remoteversion)
+        print(remote_version)
 
-        if int(version[0]) > int(remoteversion[0]):
+        # Compare version locally to what is on the remote server
+        # If the version is now greater than what is on remote server, proceed with change
+        if int(version[0]) > int(remote_version[0]):
             os.popen("sshpass -p 12345 rsync files/" + location[1] + "/" + ".version" + " cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + location[1] + "/.version")
             users = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " ls /home/cmsc626/Desktop/files/" + location[1]).read().split('\n')
-            remoteversion = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
+            remote_version = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
             # Exclude the user from the purge who just pushed the file
-            exclude = remoteversion[1]
+            exclude = remote_version[1]
             # Remove users from list of people with file who no longer have latest update
-            print("Exclude", exclude)
-            print("Users", users)
             for user in users:
                 if user != exclude and user != ".version" and user != location[1]:
                     os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " rm -f /home/cmsc626/Desktop/files/" + location[1] + "/" + user).read()
-            print("Updated the file!")
+            print("Updated file " + query + "!")
             return 1
+        # Revert changes if file version is less than what is on the directory server
         else:
             print("File has been modified. Please pull the updated version")
             downgraded = open("files/" + location[1] + "/" + ".version", "w")
-            downgraded.write(remoteversion[0] + "\n" + remoteversion[1])
+            downgraded.write(remote_version[0] + "\n" + remote_version[1])
             downgraded.close()
             return 0
     print("File does not exist")
     return 0
 
 
-def download(file):
-    location = search(file)
-    if location:
+# Check version, make changes locally, then push changes. No revert needed if wrong
+def write_v2(query, text):
+    location = search(query)
+
+    # Works with a mutex file to deal with concurrent writes
+    files = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " ls /home/cmsc626/Desktop/files/" + location[1]).read().split('\n')
+    if ".mutex" in files:
+        print("Can't modify file at this time")
+        return 0
+    else:
+        os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " touch /home/cmsc626/Desktop/files/" + location[1] + "/.mutex").read()
+
+    if location and location != 2:
+        # Get initial versions
+        version = open("files/" + location[1] + "/" + ".version").read().split()
+        remote_version = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
+
+        # If the next version number is higher than what is recorded on the directory server
+        if int(version[0])+1 > int(remote_version[0]):
+            ip = getip()
+
+            # Update the version file
+            updated = open("files/" + location[1] + "/" + ".version", "w")
+            updated.write(str(int(version[0]) + 1) + "\n" + ip)
+            updated.close()
+
+            # Update the actual text file
+            newfile = open("files/" + location[1] + "/" + location[1], "w")
+            newfile.write(text)
+            newfile.close()
+
+            # Copy the new version file to the directory server
+            os.popen("sshpass -p 12345 rsync files/" + location[1] + "/" + ".version" + " cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + location[1] + "/.version")
+            # Exclude the user from the purge who just pushed the file
+            exclude = ip
+            users = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " ls /home/cmsc626/Desktop/files/" + location[1]).read().split('\n')
+            # Purge all users who own the file because their versions are now outdated
+            for user in users:
+                if user != exclude and user != ".version" and user != location[1]:
+                    os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " rm -f /home/cmsc626/Desktop/files/" + location[1] + "/" + user).read()
+            print("Updated file " + query + "!")
+            os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " rm -f /home/cmsc626/Desktop/files/" + location[1] + "/.mutex").read()
+            return 1
+        else:
+            print("File has been previously modified. Please pull the updated version.")
+            os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " rm -f /home/cmsc626/Desktop/files/" + location[1] + "/.mutex").read()
+            return 0
+    print("File does not exist")
+    os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " rm -f /home/cmsc626/Desktop/files/" + location[1] + "/.mutex").read()
+    return 0
+
+
+def download(query):
+    location = search(query)
+    if location and location != 2:
+        # Check if user currently owns file
         if os.path.exists("files/" + location[1] + "/" + ".version"):
-            remoteversion = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
+            remote_version = os.popen("sshpass -p 12345 ssh cmsc626@" + directory_server + " cat /home/cmsc626/Desktop/files/" + location[1] + "/" + ".version").read().split("\n")
             version = open("files/" + location[1] + "/" + ".version").read().split()
             ip = getip()
-            if version[0] < remoteversion[0] and ip != remoteversion[1]:
+            # Check to see if user's version is obsolete
+            if version[0] < remote_version[0] and ip != remote_version[1]:
                 ip = getip()
-                # Combine everything cause race conditions
+                # Combine everything because of race conditions
+                # Copy file from most up to date person, copy version from directory, and insert ip to show ownership
                 os.popen("sshpass -p 12345 rsync -r cmsc626@" + location[0] + ":/home/cmsc626/Desktop/files/" + location[1] + " files/" + " && "
                          + "sshpass -p 12345 rsync cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + location[1] + "/.version" + " files/" + location[1] + "/.version" + " && "
                          + "sshpass -p 12345 ssh cmsc626@" + directory_server + " touch /home/cmsc626/Desktop/files/" + location[1] + "/" + ip).read()
+                print("File " + query + " has been successfully downloaded!")
                 return 1
+            # If user has latest version
             else:
                 print("You already have the latest version")
                 return 0
+        # If user does not have the file at all (pretty much do same as above but without version checking)
         else:
             ip = getip()
-            print(location)
-            # Combine everything cause race conditions
+            # Combine everything because of race conditions
+            # Copy file from most up to date person, copy version from directory, and insert ip to show ownership
             os.popen("sshpass -p 12345 rsync -r cmsc626@" + location[0] + ":/home/cmsc626/Desktop/files/" + location[1] + " files/" + " && "
                      + "sshpass -p 12345 rsync cmsc626@" + directory_server + ":/home/cmsc626/Desktop/files/" + location[1] + "/.version" + " files/" + location[1] + "/.version" + " && "
                      + "sshpass -p 12345 ssh cmsc626@" + directory_server + " touch /home/cmsc626/Desktop/files/" + location[1] + "/" + ip).read()
+            print("File " + query + " has been successfully downloaded!")
             return 1
     else:
         print("File not found")
@@ -151,14 +216,34 @@ def download(file):
 if __name__ == "__main__":
     getip()
     if args.search:
-        print(search(args.search))
+        file = search(args.search)
+        if file:
+            if file == 2:
+                print("Directory server not online")
+            else:
+                print("File " + str(args.search) + " has been found!")
+        else:
+            print("File " + str(args.search) + " has not been found.")
+
     elif args.read:
-        print(read(args.read))
+        file = read(args.read)
+        if file:
+            print(str(args.read) + ": " + file)
+        else:
+            print("File " + str(args.read) + " not found")
+
     elif args.create:
-        print(create(args.create))
+        file = create(args.create)
+        if file:
+            print("File " + str(args.create) + " has been created!")
+        else:
+            print("File " + str(args.create) + " could not be created.")
+
     elif args.write:
-        print(write(args.write, args.message))
+        write_v2(args.write, args.message)
+
     elif args.download:
-        print(download(args.download))
+        download(args.download)
+
     else:
-        print("Bruh")
+        print("Please enter a valid flag. Use --help to see all options.")
