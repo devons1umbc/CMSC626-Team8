c08499f185756779665bc4298e86a7b28391c32c
Jay-2704 <144267712+Jay-2704@users.noreply.github.com>
2023-12-03 23:05:20 -0500

    Time Test file
    Recording the files based on requests.

---
+++ timetest.py
@@ -0,0 +1,19 @@
+import time
+import os
+
+tests = 100
+
+start_time_read = time.time()
+for i in range(tests):
+    # Permtest.txt already exists locally with user
+    os.system("python main.py -r permtest.txt")
+end_time_read = time.time() - start_time_read
+
+start_time_write = time.time()
+for i in range(tests):
+    # Permtest.txt already exists on directory server
+    os.system("python main.py -w permtest.txt -m 'commit " + str(i) + "'")
+end_time_write = time.time() - start_time_write
+
+print("Read: " + str(start_time_read))
+print("Write: " + str(start_time_write))
